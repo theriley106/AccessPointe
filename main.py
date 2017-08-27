47089efd95889491041340c06ff9766853373620
Theriley106 <christopherlambert106@gmail.com>
2017-08-27 14:16:21 -0400

    removed test.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 import re
 from wifi import Cell, Scheme
+from subprocess import STDOUT, check_output
 
 INTERFACE = 'wlp3s0'
 
+def connectWifi(ssid, password):
+	output = check_output(cmd, stderr=STDOUT, timeout=seconds)
 def genPass(dormNumber, ssid=None):
 	res = requests.get('https://en.wikipedia.org/wiki/Campus_of_Clemson_University')
 	for network in Cell.all(INTERFACE):
